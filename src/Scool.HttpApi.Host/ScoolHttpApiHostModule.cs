6c5f841fe2a3b7bf64a567f442955bb08f1bd165
Tuanle207 <letgo237@gmail.com>
2022-02-12 17:33:57 +0700

    Fix CORS

--- src/Scool.HttpApi.Host/ScoolHttpApiHostModule.cs
+++ src/Scool.HttpApi.Host/ScoolHttpApiHostModule.cs
@@ -188,8 +188,7 @@ namespace Scool
                         //.WithAbpExposedHeaders()
                         //.SetIsOriginAllowedToAllowWildcardSubdomains()
                         .AllowAnyHeader()
-                        .AllowAnyMethod()
-                        .AllowCredentials();
+                        .AllowAnyMethod();
                 });
             });
         }
